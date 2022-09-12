39f7cada04fccb2a1dad9fb871840f2280b08a59
barrysalim <syafribarry@yahoo.com>
2022-09-12 09:50:23 +0700

    in syaa allah kelar

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+    'list': data_barang_catalog,
+    'nama': 'Syafri Barry Salim',
+    'npm' : '2106752136'
+    }
+    return render(request, "katalog.html", context)
